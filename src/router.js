42dba7ba8fa2f10b52426a29aae85c16d347b2e9
Alex Hansen <alex@alex-hansen.com>
2019-06-04 07:39:07 -0500

    WIP parameter route matching

--- src/router.js
+++ src/router.js
@@ -1,13 +1,31 @@
 import PageNotFound from "./page-not-found";
 /// Routes should be in the form of { path : component }
 /// TODO support URL params w/ lambda components, get rid of exports and test in a smarter way
+/** Constructs a router which returns a function based on the current path in the browser.
+ * @param {Object<string>} routes an object of the form { routeUrl : function }
+ * @returns {function} result a function which, when executed, provides the desired content.
+ *
+ * Paths must be preceeded with a forward slash `/`. If a parameter is in the url, it must have its
+ * own "section" between slashes, i.e. `/path/{parameter}/`, _not_ `/path{parameter}/` (although the
+ * trailing slash at the end is optional).
+ */
 const Router = (routes, notFound = PageNotFound) => {
   let URL = window.location.pathname;
-  for (route in routes) {
-    // If a route contains brackets, then it has parameters. If it has parameters, then they should be removed and added to varRoutes which contains the real route.
-    // varRoutes contains { reducedURLWithoutVarNames : routeWithVarNames }
+  let paramRoutes = {};
+  for (const route of routes) {
+    // If a route contains brackets, then it has parameters. If it has parameters, then they should be removed and added to paramRoutes which contains the real route.
+    // paramRoutes contains { routeWithoutVarNames : routeWithVarNames }
+    paramRoutes[removeParNames(route)] = route;
   }
-  return routes[URL] || routes[reduceParameterUrl(URL)] || notFound; // TODO reduce parameter url
+  return routes[URL] || matchParamPath(URL, paramRoutes, routes) || notFound; // TODO reduce parameter url
+};
+
+const removeParNames = route => {
+  // While an opening bracket still exists, keep slicing out anything between a { and a }
+  while (route.indexOf("{") >= 0) {
+    route =
+      route.slice(0, route.indexOf("{")) + route.slice(route.indexOf("{") + 1);
+  }
 };
 
 /** Takes in a route's URL and returns the variable names.
@@ -46,7 +64,6 @@ const getParameterNamesFromRoute = routeUrl => {
       }
     }
   }
-
   return parNames;
 };
 
@@ -58,22 +75,76 @@ const validateVarName = name =>
   name.indexOf("-") < 0; // TODO more things here
 
 /// Reduces a path to its potential variable-less forms
-const getPathReductions = path => {
-  let reductions = [];
-  // "While there are still non-slash characters in the path"
-  while (path.split("").filter(x => x !== "/").length > 0) {
-    // Add the path to the potential reductions and remove the bit after the last slash
-    reductions.push(path);
-    path = path.slice(0, path.lastIndexOf("/"));
-  }
-  return reductions;
-};
+/// TODO make this a generator?
+/// TODO this needs to match paths where the variables are in the beginning of the route
+/* putting this on hold as I don't want to add babel-polyfill to my dependencies...:(
+function* getPathReductions(path) {
+   let pathSections = path.split('/');
+   for (let rank = 0; rank < pathSections.length; rank++) {
+       for (let i = 0; i < pathSections.length; i++) {
+           let pathSectionsCopy = [...pathSections];
+           for (let j = 0; j < rank; j++) {
+                pathSectionsCopy[j] = "";
+           }
+           yield pathSectionsCopy.join('/');
+       }
+   }
+}*/
 
-//const getPath
+// TODO this needs to be redone
+function getPathReductions(path) {
+  let pathSections = path.split("/");
+  let reductions = [];
+  for (let rank = 0; rank < pathSections.length; rank++) {
+    for (let i = 0; i < pathSections.length; i++) {
+      let pathSectionsCopy = [...pathSections];
+      for (let j = 0; j < rank; j++) {
+        pathSectionsCopy[i + (j % pathSections.length)] = "";
+      }
+      pathSectionsCopy = pathSectionsCopy.slice(0, pathSections.length);
+      reductions = [...reductions, pathSectionsCopy.join("/")];
+    }
+  }
+  return [...new Set(reductions)];
+}
+
+/** Matches a path to one of the routes in the object, if it exists, and passes the args into it
+ * @param {string} currentPath the current path in the browser
+ * @param {Object<string, function>} paramRoutes the reduced variable routes object
+ * @returns {function} result a function that, when executed, gives the desired output
+ * TODO needs major refactor - path matching is different with and without trailing slash
+ */
+const matchParamPath = (currentPath, paramRoutes, routes) => {
+  for (const reduction of getPathReductions(currentPath)) {
+    if (paramRoutes[reduction] !== undefined) {
+      let parameters = {};
+      let routeSections = paramRoutes[reduction]
+        .split("/")
+        .filter(x => x !== "");
+      let pathSections = currentPath.split("/").filter(x => x !== "");
+      if (routeSections.length !== pathSections.length) {
+        console.error(
+          `incorrect matching: route and path have different number of sections: ${currentPath} ${
+            paramRoutes[reduction]
+          }`
+        );
+      }
+      for (const sectionIndex in routeSections) {
+        if (routeSections[sectionIndex].indexOf("{") >= 0) {
+          parameters[
+            routeSections[sectionIndex].replace("{", "").replace("}", "")
+          ] = pathSections[sectionIndex];
+        }
+      }
+      return routes[paramRoutes[reduction]](parameters);
+    }
+  }
+};
 
 export const test = {
   getParameterNamesFromRoute: getParameterNamesFromRoute,
-  getPathReductions: getPathReductions
+  getPathReductions: getPathReductions,
+  matchParamPath: matchParamPath
 };
 
 export default Router;
--- test/router.test.js
+++ test/router.test.js
@@ -1,50 +1,77 @@
 import { test } from "../lib/router";
 // TODO test actual contents of returns
-describe("Function tests", () => {
-  describe("getParameterNamesFromRoute tests", () => {
-    it("should return no variable names", () => {
-      expect(
-        test.getParameterNamesFromRoute("/testPath/notAVarName/test/").length
-      ).toBe(0);
-    });
-    it("should return a single variable name", () => {
-      expect(
-        test.getParameterNamesFromRoute("/testPath/{varName}/test/").length
-      ).toBe(1);
-    });
-    it("should return two variable names", () => {
-      expect(
-        test.getParameterNamesFromRoute(
-          "/testPath/{varName}/{varName2}/others//extra///"
-        ).length
-      ).toBe(2);
-    });
-    it("should return three variable names", () => {
-      expect(
-        test.getParameterNamesFromRoute("/{var1}/{var2}/{var3}").length
-      ).toBe(3);
-    });
-    it("should return an error due to missing preceeding slashes", () => {
-      expect(() =>
-        test.getParameterNamesFromRoute("{var1}{var2}{var3}")
-      ).toThrow();
-    });
-    it("should return an error due to mismatched brackets", () => {
-      expect(() => test.getParameterNamesFromRoute("/{var{var1}")).toThrow();
-    });
-    it("should throw an error due to improper var names", () => {
-      expect(() => test.getParameterNamesFromRoute("/{var-1}/")).toThrow();
-    });
-    it("should throw an error due to improper var names", () => {
-      expect(() => test.getParameterNamesFromRoute("/{var{1}}/")).toThrow();
-    });
+describe("getParameterNamesFromRoute tests", () => {
+  it("should return no variable names", () => {
+    expect(
+      test.getParameterNamesFromRoute("/testPath/notAVarName/test/").length
+    ).toBe(0);
   });
-  describe("getPathReductions tests", () => {
-    it("should return three reductions", () => {
-      expect(test.getPathReductions("/three/reductions/").length).toBe(3);
-    });
-    it("should return zero reductions", () => {
-      expect(test.getPathReductions("").length).toBe(0);
-    });
+  it("should return a single variable name", () => {
+    expect(
+      test.getParameterNamesFromRoute("/testPath/{varName}/test/").length
+    ).toBe(1);
+  });
+  it("should return two variable names", () => {
+    expect(
+      test.getParameterNamesFromRoute(
+        "/testPath/{varName}/{varName2}/others//extra///"
+      ).length
+    ).toBe(2);
+  });
+  it("should return three variable names", () => {
+    expect(
+      test.getParameterNamesFromRoute("/{var1}/{var2}/{var3}").length
+    ).toBe(3);
+  });
+  it("should return an error due to missing preceeding slashes", () => {
+    expect(() =>
+      test.getParameterNamesFromRoute("{var1}{var2}{var3}")
+    ).toThrow();
+  });
+  it("should return an error due to mismatched brackets", () => {
+    expect(() => test.getParameterNamesFromRoute("/{var{var1}")).toThrow();
+  });
+  it("should throw an error due to improper var names", () => {
+    expect(() => test.getParameterNamesFromRoute("/{var-1}/")).toThrow();
+  });
+  it("should throw an error due to improper var names", () => {
+    expect(() => test.getParameterNamesFromRoute("/{var{1}}/")).toThrow();
+  });
+});
+describe("getPathReductions tests", () => {
+  it("should return four reductions", () => {
+    /* /four/reductions/ /four// //reductions/ /// */
+    let reductions = test.getPathReductions("/four/reductions/");
+    expect(test.getPathReductions("/four/reductions/").length).toBe(4);
+  });
+  it("should return zero reductions", () => {
+    expect(test.getPathReductions("").length).toBe(1);
+  });
+});
+describe("matchParamPath tests", () => {
+  const paramRoutes = {
+    "/param/route//": "/param/route/{var1}",
+    "///other/param/route": "/{var1}/{var2}/other/param/route"
+  };
+
+  const routes = {
+    "/param/route/{var1}": () => 1,
+    "/{var1}/{var2}/other/param/route": ({ var1, var2 }) => var1 + var2
+  };
+  it("should return the number 1", () => {
+    console.log(test.matchParamPath("/param/route/param", paramRoutes, routes));
+    expect(
+      test.matchParamPath("/param/route/param/", paramRoutes, routes)
+    ).toBe(1);
+  });
+  it("should return the number 1", () => {
+    expect(
+      test.matchParamPath("/param/route/param/", paramRoutes, routes)
+    ).toBe(1);
+  });
+  it("should concat the string params in the path (ab)", () => {
+    expect(
+      test.matchParamPath("/a/b/other/param/route", paramRoutes, routes)
+    ).toBe("ab");
   });
 });
